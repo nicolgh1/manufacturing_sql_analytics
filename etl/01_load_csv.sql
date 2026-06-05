141f90425258dfa962c8c4f16c9e8d6de01e1bb5
Nicol Herta <nicolherta@icloud.com>
2026-06-05 20:35:11 +0100

    updated file paths

--- etl/01_load_csv.sql
+++ etl/01_load_csv.sql
@@ -1,9 +1,9 @@
-\copy raw_machines FROM '/home/gabs123/portofolio-projects/manufacturing_sql_analytics/data/raw/machines.csv' CSV HEADER;
+\copy raw_machines FROM 'data/raw/machines.csv' CSV HEADER;
 
-\copy raw_products FROM '/home/gabs123/portofolio-projects/manufacturing_sql_analytics/data/raw/products.csv' CSV HEADER;
+\copy raw_products FROM 'data/raw/products.csv' CSV HEADER;
 
-\copy raw_production_orders FROM '/home/gabs123/portofolio-projects/manufacturing_sql_analytics/data/raw/production_orders.csv' CSV HEADER;
+\copy raw_production_orders FROM 'data/raw/production_orders.csv' CSV HEADER;
 
-\copy raw_downtime_events FROM '/home/gabs123/portofolio-projects/manufacturing_sql_analytics/data/raw/downtime_events.csv' CSV HEADER;
+\copy raw_downtime_events FROM 'data/raw/downtime_events.csv' CSV HEADER;
 
-\copy raw_quality_checks FROM '/home/gabs123/portofolio-projects/manufacturing_sql_analytics/data/raw/quality_checks.csv' CSV HEADER;
+\copy raw_quality_checks FROM 'data/raw/quality_checks.csv' CSV HEADER;
